b51dd3f7bc2fcc2c2e25cd6f8e11964f168576bd
Ardy Teguh <149128324+Valonekiil@users.noreply.github.com>
2026-06-12 14:53:41 +0700

    push

--- Script/ItemSpawner.gd
+++ Script/ItemSpawner.gd
@@ -7,6 +7,25 @@ var spawned_count:int = 0
 @onready var timer: Timer = $Timer
 @onready var colshape: CollisionShape2D = $CollisionShape2D
 @onready var indicator: Label = $Label
+@export var shop_ui:CanvasLayer
 
 func _ready() -> void:
 	indicator.visible = false
+	shop_ui.visible = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_key_pressed(KEY_TAB) and player_near:
+		shop_ui.visible = !shop_ui.visible
+		print("i[t]")
+
+var player_near:bool
+
+func _on_body_entered(body: Node2D) -> void:
+	player_near = true
+	print("ada")
+
+func _on_body_exited(body: Node2D) -> void:
+	player_near = false
+	if shop_ui.visible:
+		shop_ui.visible = false
+	print("g ada")
